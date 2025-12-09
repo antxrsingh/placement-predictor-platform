55f03a9e8a7958c67babbb121238d19826e89a7f
antarpreet singh mangat <antarsingh0407@gmail.com>
2025-12-09 16:54:47 +0530

    Update API URL to Render backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -72,7 +72,8 @@ function App() {
     }
 
     try {
-      const res = await fetch("http://127.0.0.1:5000/predict", {
+      const res = await fetch("https://placement-predictor-backend.onrender.com/predict", {
+
         method: "POST",
         headers: {
           "Content-Type": "application/json"
